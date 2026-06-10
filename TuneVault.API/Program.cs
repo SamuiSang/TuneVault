ea48d0305f1fd9f0ec63feb455636f42bf8d0b9c
SamuiSang <nguyenthanhsang5613@gmail.com>
2026-06-11 05:10:39 +0700

    thiết lập JWT bearer để test API

--- TuneVault.API/Program.cs
+++ TuneVault.API/Program.cs
@@ -10,22 +10,43 @@ using TuneVault.Infrastructure.Services;
 using TuneVault.Application.Common.Interfaces;
 using TuneVault.Application.Common.Interfaces.Repositories;
 
-using System.Data; // Đã thêm dấu chấm phẩy
+using System.Data;
 using Microsoft.AspNetCore.Identity; // Bắt buộc cho Identity
 using TuneVault.Domain.Entities.Users; // Trỏ tới class AppUser của bạn
+
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDataProtection();
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi(); //XUNG ĐỘT VỚI SWAGGER
 builder.Services.AddScoped<IMediaShareRepository, MediaShareRepository>();
 builder.Services.AddScoped<INotificationRepository, NotificationRepository>();
 builder.Services.AddScoped<IMediaRepository, MediaRepository>();
 builder.Services.AddScoped<INotificationService, NotificationService>();
 // ---> SIGNALR SERVICE <---
 builder.Services.AddSignalR();
-builder.Services.AddSwaggerGen();
+//---> SWAGGER , BÁO CHO SWAGGER BIẾT RẰNG HỆ THỐNG ĐANG SỬ DỤNG XÁC THỰC BẰNG JWT BEARER <---
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Nhập 'Bearer' [khoảng trắng] và chuỗi token của bạn vào ô bên dưới.\n\nVí dụ: 'Bearer eyJhbGciOiJIUzI1NiIs...'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecuritySchemeReference("Bearer"),
+            new List<string>()
+        }
+    });
+ 
+});
 // ---> CẤU HÌNH IDENTITY CHẠY ĐỘC LẬP (KHÔNG DÙNG EF CORE) <---
 builder.Services.AddIdentityCore<AppUser>(options => {
     // Nới lỏng quy tắc mật khẩu cho dễ test
@@ -59,10 +80,10 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// ---> Configure the HTTP request PIPELINE <---
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi(); //xung đột với swagger
     app.UseSwagger();
     app.UseSwaggerUI();
 }
